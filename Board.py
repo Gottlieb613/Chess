4060f140a5708ddb41dabc471f6e93418da833d4
Gottlieb613 <charliegottlieb@gmail.com>
2022-07-13 00:18:52 +0300

    Create Board.py
    First file!

---
+++ Board.py
@@ -0,0 +1,7 @@
+# I was thinking we would have an object for the board itself
+# and maybe some for pieces and the gameplay and then the GUI. idk we'll figure it out
+
+class Board:
+    def __init__():
+        None
+    
