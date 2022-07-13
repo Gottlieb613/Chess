7bf43d03cc98ecac51c2556033aa4dba17c0383a
elijahmm <78058709+elijahmm@users.noreply.github.com>
2022-07-12 23:56:10 -0400

    chore: setup for board, piece

--- Board.py
+++ Board.py
@@ -1,7 +1,33 @@
+import numpy as np
 # I was thinking we would have an object for the board itself
 # and maybe some for pieces and the gameplay and then the GUI. idk we'll figure it out
 
 class Board:
-    def __init__():
-        None
+    state = None 
+    
+    def __init__(self):
+        self.setup_board()
+        pass
+
+    def print_board(self):
+        b = self.state
+        # {
+        #         1: ['R', 'N', 'B', 'K', 'Q', 'B', 'N', 'R'],
+        #         2: ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+        #         3: [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        #         4: [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        #         5: [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        #         6: [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        #         7: ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+        #         8: ['R', 'N', 'B', 'K', 'Q', 'B', 'N', 'R'],
+        # }
+        outline = '+---+---+---+---+---+---+---+---+'
+        for i in range(8):
+            print(outline)
+            print(f"| {b[1][i]} | {b[2][i]} | {b[3][i]} | {b[4][i]} | {b[5][i]} | {b[6][i]} | {b[7][i]} | {b[8][i]} |")
+        print(outline)
+        pass
+
+    def setup_board(self):
+        self.state = np.ndarray(size = (8,8))
     
---
+++ Piece.py
@@ -0,0 +1,43 @@
+
+
+#imports
+
+class Piece():
+    def __init__(self):
+        # TODO
+        pass
+
+    def is_valid_move(self):
+        # TODO 
+        pass
+
+class Pawn(Piece):
+    def __init__(self):
+        pass
+
+class Rook(Piece):
+    def __init__(self):
+        pass
+
+class Knight(Piece):
+    def __init__(self):
+        pass
+
+class Bishop(Piece):
+    def __init__(self):
+        pass
+
+class Queen(Piece):
+    def __init__(self):
+        pass
+
+class King(Piece):
+    def __init__(self):
+        pass
+    
+class InvisiblePawn(Piece):
+    # For en passant
+    
+    def __init__(self):
+        pass
+
